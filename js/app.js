8f6990beb4836e4dae8cc67c52d39964f63c96c6
coltonchristensen-charliepup <colton.james.christensen@gmail.com>
2020-10-15 19:34:53 -0700

    added files and content

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('hello world')
+var seattle = {
+
+  getRandom: 0,
+  min: 23,
+  max: 65,
+  avgCookieSale: 6.3,
+  numbrerCustomers: 0,
+}
+  hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm']
+
+  numberOfCookies = ['16 cookies', '20 cookies', '35 cookies', '48 cookies', '56 cookies', '77 cookies', '93 cookies', '144 cookies', '119 cookies', '84 cookies', '61 cookies', '23 cookies', '42 cookies', '57 cookies', 'Total: 875 cookies']
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  
+  console.log(getRandomInt();
+  // expected output: 0, 1 or 2
+  
+  console.log(getRandomInt(1));
+  // expected output: 0
+  
+  console.log(Math.random());
